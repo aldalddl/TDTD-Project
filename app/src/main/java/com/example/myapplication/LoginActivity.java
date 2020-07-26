9224034b8543a55edf083670b9eda9082bb34226
Minji Kang <aldalddl2007@gmail.com>
2020-07-26 19:52:32 +0900

    구글 로그인 (성공)

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,19 +2,15 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 import android.content.Intent;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -22,34 +18,101 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.annotations.Nullable;
+
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
-    public static final int RC_SIGN_IN = 1000;
-    private FirebaseAuth mFirebaseAuth;
-    private GoogleApiClient mGoogleApiClient;
-    private DatabaseReference mDatabase;
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private SignInButton btn_google; //구글 로그인 버튼
+    private FirebaseAuth auth; //파이어베이스 인증 객체
+    private GoogleApiClient googleApiClient; //구글 API 클라이언트 객체
+    public static final int REQ_SIGN_GOOGLE = 100; //구글 로그인 결과 코드
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("640610345628-uemba1caf73d5o29cvfglruskog3smgh.apps.googleusercontent.com")
+//        mFirebaseAuth = FirebaseAuth.getInstance();
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestIdToken("640610345628-uemba1caf73d5o29cvfglruskog3smgh.apps.googleusercontent.com")
+//                .requestEmail()
+//                .build();
+
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .enableAutoManage(this, this)
+//                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+//                .build();
+//        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
+        googleApiClient = new GoogleApiClient.Builder(this) //fragment-getContext
                 .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
                 .build();
-        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        auth = FirebaseAuth.getInstance(); //파이어베이스 인증 객체 초기화
+
+        btn_google = findViewById(R.id.btn_google);
+        btn_google.setOnClickListener(new View.OnClickListener() { //구글 로그인 버튼을 클릭했을 때 이곳을 수행
+            @Override
+            public void onClick(View view) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, REQ_SIGN_GOOGLE);
+            }
+        });
+
+//        findViewById(R.id.sign_in_button).setOnClickListener(this);
+    }
+
+
+//    @Override
+//    public void onClick(View v) {
+//        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+//        startActivityForResult(signInIntent, RC_SIGN_IN);
+//    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) { // 구글 로그인 인증을 요청을 했을 때 결과 값 되돌려 받는
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQ_SIGN_GOOGLE) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()) {
+                GoogleSignInAccount account = result.getSignInAccount(); //account 라는 데이터는 구글 로그인 정보를 담고 있음
+                resultLogin(account);
+            } else{
+                Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void resultLogin(final GoogleSignInAccount account){
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        auth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+                } else{
+
+                    Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                    intent.putExtra("nickName", account.getDisplayName());
+                    Intent photoUrl = intent.putExtra("photoUrl", String.valueOf(account.getPhotoUrl()));
+                    startActivity(intent);
+                    Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 
     @Override
@@ -57,44 +120,23 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if(result.isSuccess()) {
-                GoogleSignInAccount account = result.getSignInAccount(); //account라는 데이터는 구글 로그인 정보를 담고 있음
-                firebaseAuthWithGoogle(account);
-            } else {
-                Toast.makeText(this, "구글 로그인 실패", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void firebaseAuthWithGoogle(final GoogleSignInAccount account) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        mFirebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(getApplicationContext(), "인증 실패", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                            intent.putExtra("nickname", account.getEmail());
-                            intent.putExtra("photoUrl", String.valueOf(account.getPhotoUrl()));
-                            intent.putExtra("displayname", account.getDisplayName());
-                            startActivity(intent);
-                            Toast.makeText(getApplicationContext(), "인증 성공", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
+//    private void firebaseAuthWithGoogle(final GoogleSignInAccount account) {
+//        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+//        mFirebaseAuth.signInWithCredential(credential)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (!task.isSuccessful()) {
+//                            Toast.makeText(getApplicationContext(), "인증 실패", Toast.LENGTH_SHORT).show();
+//                        } else {
+//                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                            intent.putExtra("nickname", account.getEmail());
+//                            intent.putExtra("photoUrl", String.valueOf(account.getPhotoUrl()));
+//                            intent.putExtra("displayname", account.getDisplayName());
+//                            startActivity(intent);
+//                            Toast.makeText(getApplicationContext(), "인증 성공", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+//    }
 }
---
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private TextView tv_result;
+    private ImageView iv_profile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        String nickName = intent.getStringExtra("nickName");
+        String photoUrl = intent.getStringExtra("photoUrl");
+
+        tv_result = findViewById (R.id.tv_result);
+        tv_result.setText(nickName);
+
+        iv_profile  = findViewById (R.id.iv_profile);
+        Glide.with(this).load(photoUrl).into(iv_profile);
+
+    }
+
+}
